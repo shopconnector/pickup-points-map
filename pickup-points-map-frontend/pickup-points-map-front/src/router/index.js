2d6796401c899c2eaca55ff7fefca60dffe0ba1f
Mykola Zahanych <35174437+MykolaZahanych@users.noreply.github.com>
2019-06-19 17:54:41 +0200

    Add dependencies to the project

--- pickup-points-map-frontend/pickup-points-map-front/src/router/index.js
+++ pickup-points-map-frontend/pickup-points-map-front/src/router/index.js
@@ -5,11 +5,8 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', name: 'HelloWorld', component: HelloWorld }
   ]
 })
