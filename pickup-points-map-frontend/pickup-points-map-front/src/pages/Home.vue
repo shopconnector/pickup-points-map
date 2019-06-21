2f1f0d226a608c96d591c024b68020e29c014699
Mykola Zahanych <35174437+MykolaZahanych@users.noreply.github.com>
2019-06-21 15:59:14 +0200

    Add css and "Wybierz dostawców" component

---
+++ pickup-points-map-frontend/pickup-points-map-front/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home-page">
+      <SelectSuppliers/>
+    </div>
+</template>
+
+<script>
+import router from 'vue-router'
+import vueHeadful from 'vue-headful'
+import SelectSuppliers from '@/components/SelectSuppliers.vue'
+
+export default {
+  components: {
+    router,
+    vueHeadful,
+    SelectSuppliers
+  },
+  data () {
+    return {
+
+    }
+  },
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
