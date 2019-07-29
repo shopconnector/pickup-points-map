3d7f296b0f1be9697f28e39068cd127e0bcb5f75
Dima <dmytro.kravchenko@beecommerce.pl>
2019-07-29 11:38:39 +0200

    say hi to parent
    say hi to parent

--- pickup-points-map-frontend/pickup-points-map-front/src/components/features/Filters.vue
+++ pickup-points-map-frontend/pickup-points-map-front/src/components/features/Filters.vue
@@ -3,6 +3,7 @@
       <!-- Select suppliers first version -->
       <div class="suppliers" v-if="isWidgetVersion && !isMobile">
         <h2 class="title-supp">Wybierz dostawców</h2>
+        <button id="message_button">Hi parent</button>
         <div class="suppliers-menu">
           <div class="selectSuppliers" v-for="supp in suppliers" :key="supp.id">
             <input class="styled-checkbox" type="checkbox" :id="supp.id" :value="supp.value" @click="selectedFilter()" v-model="filters.checkedSuppliers">
@@ -12,7 +13,8 @@
       </div>
       <!-- Select suppliers second version -->
       <div v-if="!isWidgetVersion || isMobile">
-        <h2 class="title-dostawcow">Wybierz dostawców</h2>
+        <h2 class="title-dostawcow" id="results">Wybierz dostawców</h2>
+        <button id="message_button" @click="test2()">Hi parent</button>
         <div class="suppliers-menu-dostawcow">
           <div class="select-suppliers-dostawcow" v-for="supp in suppliers" :key="supp.id">
             <input class="styled-checkbox-dostawcow" type="checkbox" :id="supp.id" :value="supp.value" @click="selectedFilter()" v-model="filters.checkedSuppliers">
@@ -135,6 +137,16 @@ export default {
     }
   },
   methods: {
+    // ----------------------
+    sendMessage (msg) {
+      window.parent.postMessage(msg, '*')
+    },
+    test2 () {
+      var random = Math.random()
+      console.log('test2 called', random)
+      this.sendMessage('' + random)
+    },
+    // ----------------------
     selectedFilter () {
       this.$store.commit('newStoreFilters', this.filters)
     },
